ca5035a3b8c351cdb4b84663f55c83e77fc9ec1f
Marek Krupa <marek.krupa.mk@gmail.com>
2022-09-19 19:27:56 +0200

    Update tests preparing for SZP002
    - tests look for JPEG format only function

--- tests/test_files_handling.py
+++ tests/test_files_handling.py
@@ -15,13 +15,20 @@ def mock_files(request):
     remove_files(files)
 
 
+def filter_only_jpeg(mock_files):
+    file_filter = ['jpg', 'jpeg']
+    return [file for file in mock_files if file[-3:] in file_filter ]
+
+
 @pytest.mark.parametrize("mock_files", [
     [],
-    ["test_file_long_name_with_numbers12345"],
-    ["test", "test.jpg", "1234.jpg"]
+    ["test_file_long_name_with_numbers12345", "test_file_long_name_with_numbers12345.jpeg"],
+    ["test", "test.jpg", "1234.jpg", "test.bmp"],
+    ["1.jpg", "2.jpg", "3.jpg", "jpg.jpg.bmp", "bmp.jpg.jpg"]
 ], indirect=True)
-def test_file_discovery(mock_files):
+def test_file_discovery_jpeg_only(mock_files):
     actual_files = discover_files(get_test_data_path())
+    jpeg_files = filter_only_jpeg(mock_files)
 
     assert isinstance(actual_files, list)
-    assert Counter(mock_files) == Counter(actual_files)
+    assert Counter(jpeg_files) == Counter(actual_files)
