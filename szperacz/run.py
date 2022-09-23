894ad057dbb94a0d43da3fadcc66268df4f3f401
Marek Krupa <marek.krupa.mk@gmail.com>
2022-09-23 19:52:19 +0200

    Add test run execution param
    - run szperacz in test_data if -t or --test-run is specified

--- szperacz/run.py
+++ szperacz/run.py
@@ -1,11 +1,21 @@
 # Szperacz 2022
 # Main repo: https://github.com/vendral/szperacz
+import sys
 
 from szperacz.files import discover_files
+from tests import get_test_data_path
 
+TEST_RUN_ARG = '-t'
+TEST_RUN_PARAM = '--test-run'
 
 if __name__ == '__main__':
 
-    files = discover_files()
+    path = None
+    args = sys.argv[1:]
+
+    if TEST_RUN_ARG in args or TEST_RUN_PARAM in args:
+        path = get_test_data_path()
+
+    files = discover_files(path)
 
     print(files)
