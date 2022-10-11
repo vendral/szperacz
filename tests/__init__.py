8fabeab8170cd09e88e06af4d4dec6b315b2f99b
Marek Krupa <marek.krupa.mk@gmail.com>
2022-10-11 10:32:59 +0200

    Update tests and refactor FileHandler
    - temp tests removed
    - FileHandler refactored

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,13 @@
 import os
 from collections import Counter
-from datetime import datetime
 
-# from szperacz.files import discover_files
+from szperacz.files import FileHandler
 
 # region Global variables
-TEMP_DATA_PATH = 'temp_data/'
 TEST_DATA_PATH = 'test_data/'
 # endregion
+
+file_handler = FileHandler()
 
 
 # region Points of Control
@@ -15,43 +15,8 @@ def get_tests_dir():
     return os.path.abspath(__file__).rstrip("__init__.py")
 
 
-def get_temp_data_path():
-    return get_tests_dir() + TEMP_DATA_PATH
-
-
 def get_test_data_path():
     return get_tests_dir() + TEST_DATA_PATH
-
-
-def create_temp_data_dir():
-    try:
-        os.mkdir(get_temp_data_path())
-    except FileExistsError:
-        print('Test data folder exists')
-
-
-def remove_temp_data_dir():
-    os.rmdir(get_temp_data_path())
-
-
-def create_files(files):
-    create_temp_data_dir()
-    for file in files:
-        file = get_temp_data_path() + file
-        try:
-            with open(file, 'x') as f:
-                f.write('Mock file created: %'.format(datetime.now()))
-        except FileExistsError:
-            print('Mock file exists: '.format(file))
-
-
-def remove_files(files):
-    for file in files:
-        file = get_temp_data_path() + file
-        if os.path.exists(file):
-            os.remove(file)
-    remove_temp_data_dir()
-# endregion
 
 
 # region Points of Observation
@@ -60,13 +25,9 @@ def filter_only_jpeg(test_files):
     return [file for file in test_files if file[-3:] in file_filter or file[-4:] in file_filter]
 
 
-def check_jpeg_files_only(test_files, test_run=True):
-    if test_run:
-        # Test run in test_data/
-        actual_files = discover_files(get_test_data_path())
-    else:
-        # Temp run in generated temp_data/
-        actual_files = discover_files(get_temp_data_path())
+def check_jpeg_files_only(test_files):
+    file_handler.process_files(get_test_data_path())
+    actual_files = file_handler.images
     expected_files = filter_only_jpeg(test_files)
 
     assert isinstance(actual_files, list)
--- tests/test_files_handling.py
+++ tests/test_files_handling.py
@@ -1,16 +1,4 @@
-import pytest
-
 from tests import check_jpeg_files_only
-
-
-@pytest.mark.parametrize("temp_files", [
-    [],
-    ["test_file_long_name_with_numbers12345", "test_file_long_name_with_numbers12345.jpeg"],
-    ["test", "test.jpg", "1234.jpg", "test.bmp"],
-    ["1.jpg", "2.jpg", "3.jpg", "jpg.jpg.bmp", "bmp.jpg.jpg"]
-], indirect=True)
-def test_file_discovery_jpeg_only_temp(temp_files):
-    check_jpeg_files_only(temp_files, False)
 
 
 def test_file_discovery_jpeg_only_test(test_files):
