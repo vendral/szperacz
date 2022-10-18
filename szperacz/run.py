3b465018be6515e307bb86f0ecc5767dd33db8fa
Marek Krupa <marek.krupa.mk@gmail.com>
2022-10-18 15:26:32 +0200

    Add initial UI scaffoldings
    - Add basic Flask handling
    - Split headless and headful executions
    - Add index.html template
    - Add data.json to .gitignore
    - Update requirements

--- szperacz/run.py
+++ szperacz/run.py
@@ -3,13 +3,35 @@
 import json
 import sys
 
-from szperacz.files import FileHandler
+from flask import Flask, render_template
+
+from files import FileHandler
 from tests import get_test_data_path
 
 TEST_RUN_ARG = '-t'
 TEST_RUN_PARAM = '--test-run'
 
+HEADFUL_ARG = '-h'
+HEADFUL_PARAM = '--headful'
 
+app = Flask(__name__)
+search_path = None
+
+
+def run_search(search_path):
+    file_handler = FileHandler()
+    return file_handler.process_files(search_path)
+
+
+# region Routes
+@app.route('/')
+def index():
+    files = run_search(search_path)
+    return render_template('index.html', files_size=len(files), files=files)
+# endregion
+
+
+# region Logging
 def log_to_console(files):
     print(f'Images found: {len(files)}')
     if len(files) > 0:
@@ -21,18 +43,30 @@ def log_to_console(files):
 def log_to_file(files):
     with open('data.json', 'w') as json_file:
         json.dump(files, json_file)
+# endregion
+
+
+# region UI
+def headless(search_path):
+    files = run_search(search_path)
+    log_to_console(files)
+    log_to_file(files)
+
+
+def headful(app):
+    app.secret_key = 'SZPERACZ_KEY'
+    app.run()
+# endregion
 
 
 if __name__ == '__main__':
 
-    path = None
     args = sys.argv[1:]
 
-    file_handler = FileHandler()
-
     if TEST_RUN_ARG in args or TEST_RUN_PARAM in args:
-        path = get_test_data_path()
+        search_path = get_test_data_path()
 
-    files = file_handler.process_files(path)
-    log_to_console(files)
-    log_to_file(files)
+    if HEADFUL_ARG in args or HEADFUL_PARAM in args:
+        headful(app)
+    else:
+        headless(search_path)
