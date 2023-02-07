81b6b933c1fef7b83e9f2cdcd9b40547af44fb9f
Jarogniev <102618797+Jarogniev@users.noreply.github.com>
2023-02-07 19:06:25 +0100

    SZP013 Almost done
    - added property 'log_level', 'get_log_level' method and logging levels
    - added some 'logger.error' method in 'run.py' to test 'Logger'
    - fixed 'loger.error' method to write to 'error_log.txt' file, all error announcement

--- szperacz/logger.py
+++ szperacz/logger.py
@@ -1,18 +1,36 @@
 
 
 class Logger:
+    global log_level
+    log_level = 0
+    level = log_level
     DEBUG = '[DEBUG]'
     INFO = '[INFO]'
     ERROR = '[ERROR]'
 
+    error = open("error_log.txt", mode='w')
+
+    def set_log_level(self):
+        pass
+
+    def get_log_level(self):
+        return print(f'Logging level: {self.level}')
+
     def debug(self, msg):
-        return print(f'{self.DEBUG} {msg}')
+        if log_level == 0:
+            return print(f'{self.DEBUG} {msg}')
 
     def info(self, msg):
-        return print(f'{self.INFO} {msg}')
+        if log_level <= 1:
+            return print(f'{self.INFO} {msg}')
 
     def error(self, msg):
-        return print(f'{self.ERROR} {msg}')
+        if log_level <= 2:
+            error = open("error_log.txt", mode='a')
+            er = f'{self.ERROR} {msg}' '\n'
+            error.writelines(er)
+            error.close()
+            return print(f'{self.ERROR} {msg}')
 
 
 logger = Logger()
