6f2823eb5bcceb1418c3995260471e6fa59abe2d
Jarogniev <102618797+Jarogniev@users.noreply.github.com>
2023-02-24 16:33:02 +0100

    SZP013 Done

--- szperacz/logger.py
+++ szperacz/logger.py
@@ -1,36 +1,42 @@
 
 
 class Logger:
-    global log_level
-    log_level = 0
-    level = log_level
+    log_level = None
     DEBUG = '[DEBUG]'
     INFO = '[INFO]'
     ERROR = '[ERROR]'
+    LOG_LEVEL_DEBUG = 0
+    LOG_LEVEL_INFO = 1
+    LOG_LEVEL_ERROR = 2
 
-    error = open("error_log.txt", mode='w')
+    def __init__(self, level):
+        self.log_level = level
 
     def set_log_level(self):
-        pass
+        log_levels = [0, 1, 2]
+        if self.log_level in log_levels:
+            pass
+        else:
+            raise Exception(f'Current log level {self.log_level} is out of range "0-2"')
 
     def get_log_level(self):
-        return print(f'Logging level: {self.level}')
+        print(f'Logging level: {self.log_level}')
 
     def debug(self, msg):
-        if log_level == 0:
-            return print(f'{self.DEBUG} {msg}')
+        if self.log_level == self.LOG_LEVEL_DEBUG:
+            print(f'{self.DEBUG} {msg}')
 
     def info(self, msg):
-        if log_level <= 1:
-            return print(f'{self.INFO} {msg}')
+        if self.log_level <= self.LOG_LEVEL_INFO:
+            print(f'{self.INFO} {msg}')
 
     def error(self, msg):
-        if log_level <= 2:
-            error = open("error_log.txt", mode='a')
-            er = f'{self.ERROR} {msg}' '\n'
-            error.writelines(er)
-            error.close()
-            return print(f'{self.ERROR} {msg}')
+        if self.log_level <= self.LOG_LEVEL_ERROR:
+            with open("error_log.txt", mode='a') as error_log:
+                er = f'{self.ERROR} {msg}' '\n'
+                error_log.write(er)
+            print(f'{self.ERROR} {msg}')
 
 
-logger = Logger()
+logger = Logger(0)
+logger.set_log_level()
