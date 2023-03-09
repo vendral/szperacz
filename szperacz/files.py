a12557d0b8a45d721301eabfbcb2492314235117
Jarogniev <102618797+Jarogniev@users.noreply.github.com>
2023-03-09 20:52:24 +0100

    SZP014 Done
    - Created 'config.ini' file
    - Created a field 'LOG_LEVEL'in 'config.ini'

--- szperacz/files.py
+++ szperacz/files.py
@@ -31,7 +31,6 @@ class FileHandler:
         except AttributeError:
             data['creation_time'] = None
 
-
     def _extract_exif(self):
         for idx, data in enumerate(self.data):
             with open(data['path'], "rb") as img:
