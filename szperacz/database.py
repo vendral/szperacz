f64c27b93001aa8b1728d68c41ebff7cd26f1621
Jarogniev <102618797+Jarogniev@users.noreply.github.com>
2023-06-20 20:11:00 +0200

    SZP016 Done
    - In location szperacz/ add "database.py" file with class "Database"
    - create a four def "create", "read", "update", "delete" in class "Database" body
    - some issues are fixed from SZP015

---
+++ szperacz/database.py
@@ -0,0 +1,20 @@
+
+class Database:
+
+    def __init__(self):
+        self.create()
+        self.read()
+        self.update()
+        self.delete()
+
+    def create(self):
+        create = None
+
+    def read(self):
+        read = None
+
+    def update(self):
+        update = None
+
+    def delete(self):
+        delete = None
